6773783c2977a15f70fae5dad72b0510f1cb268b
Kawsar-Ahmed-Ridoy <kawsarahmedridoy1@gmail.com>
2022-11-25 18:01:51 +0600

    more information extra section added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import More from '../More/More';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <More></More>
         </div>
     );
 };
---
+++ src/Pages/Home/More/More.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const More = () => {
+  return (
+    <div className="mb-16">
+      <h1 className="text-center text-4xl font-bold mb-8">More</h1>
+
+      <div data-theme="black" className="py-8 shadow-md  opacity-90">
+        <h2 className="text-center text-5xl font-mono font-extrabold mt-6 ">
+          Buy second hand bike <br/> online at <br/> Affordable Price
+        </h2>
+
+        <p className="text-center text-lg w-1/2 mx-auto my-6">
+          There is growing popularity of the second hand bike market in India.
+          The majority of the commoners plan to buy second hand bikes for easy
+          transportation to combat city traffic. Both in performance and
+          reliability, these are a great option to buy as it doesn't suffer from
+          much depreciation while you are planning to sell them off! Used bikes
+          for sale are in good condition. The two-wheeler enthusiasts in India
+          are eager to buy used bikes at a reasonable price from CredR. They
+          look for the best deal to buy their dream motorcycles. There are
+          numerous collections of second hand motorbikes for sale online. You
+          can choose second hand bikes from the most popular brands easily. Many
+          people step back as they don't get their preferred bikes because of
+          the high prices. If you plan to take a chance with the second hand
+          two-wheeler, you'll benefit in both quality and price.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default More;
